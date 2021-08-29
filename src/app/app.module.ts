8eb75b31899fe6d57df7251ee48ebe45059d243a
Sailesh Patra <saileshpatra997@gmail.com>
2021-08-29 10:15:32 +0530

    Using one instance of accounts-service application-wide

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AccountComponent } from './account/account.component';
 import { NewAccountComponent } from './new-account/new-account.component';
+import { AccountsService } from './accounts.service';
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +17,7 @@ import { NewAccountComponent } from './new-account/new-account.component';
     BrowserModule,
     FormsModule,
   ],
-  providers: [],
+  providers: [AccountsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -5,7 +5,7 @@ import {AccountsService} from '../accounts.service';
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService,AccountsService]
+  providers: [LoggingService]
 })
 export class NewAccountComponent {
 /*   @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
